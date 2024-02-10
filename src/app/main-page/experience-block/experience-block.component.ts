7d0ae2f52e9cd761bc0d24df6c15cf1fa4e9b72d
Zhur06 <zhur06@gmail.com>
2024-02-10 19:49:11 +0300

    + Experience block

--- src/app/main-page/experience-block/experience-block.component.ts
+++ src/app/main-page/experience-block/experience-block.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { IExperienceDescription } from './iexperience-description';
+import { CvBlockAppearenceComponent } from '../cv-block-appearence/cv-block-appearence.component';
 
 @Component({
   selector: 'app-experience-block',
   standalone: true,
-  imports: [],
   templateUrl: './experience-block.component.html',
-  styleUrl: './experience-block.component.scss',
+  styleUrls: ['./experience-block.component.scss', '../main-page.component.scss'],
+  imports: [CvBlockAppearenceComponent],
 })
-export class ExperienceBlockComponent {}
+export class ExperienceBlockComponent {
+  @Input({ required: true }) previousExperiences!: IExperienceDescription[];
+}
---
+++ src/app/main-page/experience-block/iexperience-description.ts
@@ -0,0 +1,7 @@
+export interface IExperienceDescription {
+  title: string;
+  company: string;
+  startDate: string;
+  endDate?: string;
+  description: string;
+}
