3a8161175be1f5b05f9027e843f50ae8cc8dfdf5
Zhur06 <zhur06@gmail.com>
2024-02-11 15:18:50 +0300

    + Recomendations cv block

---
+++ src/app/main-page/recomendations-block/irecomendation.ts
@@ -0,0 +1,5 @@
+export interface IRecomendation {
+  source: string;
+  sourceTag: string;
+  text: string;
+}
--- src/app/main-page/recomendations-block/recomendations-block.component.ts
+++ src/app/main-page/recomendations-block/recomendations-block.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { CvBlockAppearenceComponent } from '../cv-block-appearence/cv-block-appearence.component';
+import { BadgeComponent } from '../../shared/badge/badge.component';
+import { IRecomendation } from './irecomendation';
 
 @Component({
   selector: 'app-recomendations-block',
   standalone: true,
-  imports: [],
   templateUrl: './recomendations-block.component.html',
-  styleUrl: './recomendations-block.component.scss',
+  styleUrls: ['./recomendations-block.component.scss', '../main-page.component.scss'],
+  imports: [CvBlockAppearenceComponent, BadgeComponent],
 })
-export class RecomendationsBlockComponent {}
+export class RecomendationsBlockComponent {
+  @Input({ required: true }) recomendations!: IRecomendation[];
+}
