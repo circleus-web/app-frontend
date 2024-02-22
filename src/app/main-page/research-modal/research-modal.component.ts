3a9567b4e105d55797e888cdcdca781c0d93cbee
Zhur06 <zhur06@gmail.com>
2024-02-22 10:32:09 +0300

    + Possibility calculation modal content

--- src/app/main-page/research-modal/research-modal.component.ts
+++ src/app/main-page/research-modal/research-modal.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, TemplateRef } from '@angular/core';
 
 import { ModalComponent } from '../modal/modal.component';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-research-modal',
@@ -9,7 +10,7 @@ import { ModalComponent } from '../modal/modal.component';
   templateUrl: './research-modal.component.html',
   styleUrl: './research-modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, ModalComponent],
+  imports: [CommonModule, ModalComponent, ButtonComponent],
 })
 export class ResearchModalComponent {
   protected m_myPossibilityTemplate!: TemplateRef<Element>;
