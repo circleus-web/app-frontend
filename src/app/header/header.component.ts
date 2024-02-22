eb64638346f99d1dbcdb309fba03667e13d24fdf
Zhur06 <zhur06@gmail.com>
2024-02-22 22:51:08 +0300

    + Modal in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected m_userDropdownActive: WritableSignal<boolean> = signal(false);
+
+  protected m_onUserClick() {
+    this.m_userDropdownActive.set(!this.m_userDropdownActive());
+  }
+}
